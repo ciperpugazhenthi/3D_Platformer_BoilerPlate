452feb3b9eae041f155df3cc0e64fb4868cbd8cd
Pugazhenthi Ramakrishnan <pugazhdhana@gmail.com>
2025-08-02 00:40:57 +0530

    Revert "Fixed the inertia problem in player controller added smooth as ease in motion for player movement with physics based movement and fixed the Fuel canister spawner script now the fuel canister spawns withing the playable area. updated player prefab with adequate setting for smooth player movement"
    This reverts commit 0536a796637f78387f9b37131e6bdb917b97fe26.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -7,15 +7,6 @@ public class PlayerController : MonoBehaviour
     [Header("Thruster Settings")]
     public float thrusterForce = 10f;
     public float jumpForce = 15f;
-    [SerializeField] private float airDrag = 1.5f;
-    [SerializeField] private float groundedDrag = 5f;
-    [SerializeField] private float maxSpeed = 10f;
-
-    [Header("Acceleration Settings")]
-    [SerializeField] private AnimationCurve accelerationCurve = AnimationCurve.Linear(0, 0, 1, 1);
-    [SerializeField] private float accelerationMultiplier = 1f;
-    private float currentThrustLerp = 0f;
-
 
     [Header("Fuel Settings")]
     public float maxFuel = 100f;
@@ -39,83 +30,56 @@ public class PlayerController : MonoBehaviour
     [SerializeField]  private bool isGrounded;
 
     private Rigidbody rb;
-    private bool dashKeyCheck = false, jumpKeyCheck = false;
 
-    float hInput, vInput;
 
-    private void Start()
+    void Start()
     {
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true; // Prevents unwanted rotation during collisions
         currentFuel = maxFuel;
     }
 
-    private void Update()
-    {
-        if (Input.GetKeyDown(dashKey))
-            dashKeyCheck = true;
-        if (Input.GetKeyUp(dashKey))
-            dashKeyCheck = false;
-
-        if (Input.GetButtonDown("Jump"))
-            jumpKeyCheck = true;
-        if (Input.GetButtonUp("Jump"))
-            jumpKeyCheck = false;
-
-        hInput = Input.GetAxis("Horizontal");
-        vInput = Input.GetAxis("Vertical");
-    }
     void FixedUpdate()
     {
+        // Ground check
         isGrounded = Physics.CheckSphere(groundCheck.position, groundCheckRadius, groundLayer);
+
+        float hInput = Input.GetAxis("Horizontal");
+        float vInput = Input.GetAxis("Vertical");
+        //float downThrust = Mathf.Min(vInput, 0f); // Only downward when pressing S or stick down
 
         Vector3 thrustDirection = new Vector3(hInput, vInput, 0f);
         bool isThrusting = thrustDirection.magnitude > 0.01f;
 
-        rb.drag = isGrounded ? groundedDrag : airDrag;
-        Debug.Log($"thrust direction {thrustDirection} is thrustin {isThrusting}");
-
-        if (!isGrounded && isThrusting && currentFuel > 0f)
-        {
-            // Increase lerp factor when thrusting
-            currentThrustLerp += Time.fixedDeltaTime * accelerationMultiplier;
-            currentThrustLerp = Mathf.Clamp01(currentThrustLerp);
-        }
-        else
-        {
-            // Decay lerp factor when not thrusting
-            currentThrustLerp -= Time.fixedDeltaTime * accelerationMultiplier;
-            currentThrustLerp = Mathf.Clamp01(currentThrustLerp);
-        }
-
-        float thrustFactor = accelerationCurve.Evaluate(currentThrustLerp);
-        rb.AddForce(thrustDirection * thrusterForce * thrustFactor);
-        if (isThrusting)
-        {
-            currentFuel -= fuelConsumptionRate * Time.fixedDeltaTime * thrustFactor;
-        }
-
-
-        if (rb.velocity.magnitude > maxSpeed)
-        {
-            rb.velocity = rb.velocity.normalized * maxSpeed;
-        }
-
-        if (dashKeyCheck && canDash && !isDashing)
+        // Dash input with fuel check
+        if (Input.GetKeyDown(dashKey) && canDash && !isDashing)
         {
             if (currentFuel >= dashFuelCost)
             {
                 currentFuel -= dashFuelCost;
                 StartCoroutine(Dash());
             }
+            else
+            {
+                Debug.Log("Not enough fuel to dash!");
+            }
         }
 
+        // Apply thruster force in air
+        if (!isGrounded && isThrusting && currentFuel > 0f)
+        {
+            rb.AddForce(thrustDirection * thrusterForce);
+            currentFuel -= fuelConsumptionRate * Time.fixedDeltaTime;
+            currentFuel = Mathf.Clamp(currentFuel, 0f, maxFuel);
+        }
+
+        // Fuel recharge and jump when grounded
         if (isGrounded)
         {
             currentFuel += fuelRechargeRate * Time.fixedDeltaTime;
             currentFuel = Mathf.Clamp(currentFuel, 0f, maxFuel);
 
-            if (jumpKeyCheck)
+            if (Input.GetButtonDown("Jump"))
             {
                 rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             }
@@ -126,6 +90,7 @@ public class PlayerController : MonoBehaviour
         currentFuel += amount;
         currentFuel = Mathf.Clamp(currentFuel, 0f, maxFuel);
     }
+
 
     private IEnumerator Dash()
     {
@@ -146,3 +111,85 @@ public class PlayerController : MonoBehaviour
         canDash = true;
     }
 }
+
+
+/*using UnityEngine;
+using System;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 6f;
+    public float jumpHeight = 2f;
+    public float gravity = -9.81f;
+    public Transform cameraTransform;
+    public Vector3 oldInput = Vector3.zero;
+
+    public float fireRate = 0.1f;
+    public float shootRange = 10000f;
+
+    public bool showDebugRay = true;
+    public float debugRayDuration = 0.5f;
+
+    private CharacterController controller;
+    private Vector3 velocity;
+    private bool isGrounded;
+    private float nextFireTime = 0f;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        HandleMovement();
+        HandleShooting();
+    }
+
+    void HandleMovement()
+    {
+
+    }
+
+    void HandleShooting()
+    {
+        if (Input.GetButton("Fire1") && Time.time >= nextFireTime)
+        {
+            Shoot();
+            nextFireTime = Time.time + fireRate;
+        }
+    }
+
+    void Shoot()
+    {
+        Vector3 mouseScreenPosition = Input.mousePosition;
+        mouseScreenPosition.z = Camera.main.WorldToScreenPoint(transform.position).z;
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(mouseScreenPosition);
+        Vector3 rayDirection = (mousePosition - transform.position).normalized;
+
+        RaycastHit hit;
+        bool didHit = Physics.Raycast(transform.position, rayDirection, out hit, 300, -1);
+        if (didHit)
+        {
+            Debug.DrawLine(transform.position, hit.point, Color.red, debugRayDuration);
+        }
+        else
+        {
+            Debug.DrawLine(transform.position, transform.position + rayDirection * 300, Color.red, debugRayDuration);
+        }
+        OnShoot(); // Recoil, screen shake
+    }
+
+    void OnHitTarget(RaycastHit2D hit)
+    {
+        Debug.Log($"Hit: {hit.collider.name} at {hit.point}");
+
+
+    }
+
+    void OnShoot()
+    {
+        Debug.Log("Shot fired!");
+    }
+}*/
